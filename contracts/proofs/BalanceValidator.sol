def684843449738e84972a546a379bcff7e12683
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-07-18 15:17:36 +0530

    added balance validator

---
+++ contracts/proofs/BalanceValidator.sol
@@ -0,0 +1,34 @@
+// check if output from prev is input to next 
+pragma solidity ^0.4.23;
+
+import "../mixin/RootChainValidator.sol";
+import "../lib/RLP.sol";
+import "../lib/BytesLib.sol";
+
+
+contract BalanceValidator is RootChainValidator {
+    using RLP for bytes;
+    using RLP for RLP.RLPItem;
+    using RLP for RLP.Iterator;
+
+    // input transactions 
+    function validateBalance(bytes tx1, bytes tx2) public returns(bool){
+        RLP.RLPItem[] memory txData1 = tx1.toRLPItem().toList();
+        RLP.RLPItem[] memory txData2 = tx2.toRLPItem().toList();
+        RLP.RLPItem[] memory item1 = txData1[9].toData().toRLPItem().toList();
+        RLP.RLPItem[] memory item2 = txData2[9].toData().toRLPItem().toList();
+        return validateParams(BytesLib.toUint(item1[2].toData(), 64), 
+                              BytesLib.toUint(item1[2].toData(), 96),
+                                BytesLib.toUint(item2[2].toData(), 0),
+                                BytesLib.toUint(item2[2].toData(), 32)            
+                                );
+        
+    }
+
+    function validateParams(uint256 input1, uint256 input2, uint256 output1, uint256 output2) returns (bool) {
+        if ((input1+input2) == (output1+output2))
+            return true;
+        else
+            return false;
+    }
+}
