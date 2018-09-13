d1074038a266f3b95370e14cd30f21b4c4900914
ashishrp <a4ashishrp@gmail.com>
2018-09-13 13:50:35 +0530

    clean up/lint use uint8 in for

--- contracts/lib/validator_set.sol
+++ contracts/lib/validator_set.sol
@@ -1,66 +1,67 @@
-contract validators {
+pragma solidity ^0.4.24;
 
-struct validator{
-  int stake;
-  int acc;
-  uint256 id;
-}
 
-uint256 count=0;
-bool flag = false;
-int sum = 0;
-validator[] validators;
+contract ValidatorSet {
 
-function add(int stake) {
-  validators.push(validator(stake, stake, count));
-  sum+=stake;
-  count+=1;
-}
+  struct Validator {
+    int stake;
+    int acc;
+    uint256 id;
+  }
 
-function getProposers() returns(uint256){
-    
-    for(uint256 i=0; i<validators.length; i++){
-        validators[i].acc += validators[i].stake; 
+  uint256 count = 0;
+  bool flag = false;
+  int sum = 0;
+  Validator[] validators;
+
+  function addValidator(int stake) public {
+    validators.push(Validator(stake, stake, count));
+    sum += stake;
+    count += 1;
+  }
+
+  function getProposers() public returns(uint256){
+    require(validators.length > 0);
+    if (!flag) {
+      flag = true;
+      return getProposer();
+    }
+
+    for (uint8 i = 0; i<validators.length; i++) {
+      validators[i].acc += validators[i].stake; 
     }
     return getProposer();
-}
+  }
 
-function getProposer()returns (uint256){
+  function getProposer() private returns (uint256){
     int max = -99999999999999;//  use -ve max
-    uint256 index=0;
-    for(uint256 i=0; i<validators.length; i++){
-        if (max < validators[i].acc){
-            max =validators[i].acc;
-            index = i;
-        }
+    uint8 index = 0;
+    for (uint8 i = 0; i<validators.length; i++) {
+      if (max < validators[i].acc){
+        max = validators[i].acc;
+        index = i;
+      }
     }
     validators[index].acc -= sum;
     return validators[index].id;
-}
+  }
 
-// test pilot 
-function getNgetProposers(uint256 n) returns(uint256[]){
-    add(4);
-    add(2);
-    add(2);
-    add(2);
+  // test pilot 
+  function getNgetProposers(uint256 n) public returns(uint256[]) {
+    addValidator(4);
+    addValidator(2);
+    addValidator(2);
+    addValidator(2);
     uint256[] proposers;
-    proposers.push(getProposer());
-    for(uint256 i=1;i<n;i++){
-       proposers.push(getProposers());
+
+    for (uint256 i = 0; i<n; i++) {
+      proposers.push(getProposers());
     }
     return proposers;
-}
-/*
-getNgetProposers(20);
-0,1,2,3,0,0,1,2,3,0,0,1,2,3,0,0,1,2,3,0
-getProposer();// just one time
-getProposers()
+  }
 
-
-
-*/
-
-
-
-}
+  /*getNgetProposers(20);
+    0,1,2,3,0,0,1,2,3,0,0,1,2,3,0,0,1,2,3,0
+    getProposer();// just one time
+    getProposers() */
+  }
